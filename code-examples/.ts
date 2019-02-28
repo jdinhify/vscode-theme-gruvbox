d9b865935787489b94da0eca9b52747bb20d175d
jdinhify <jdinhify@gmail.com>
2019-03-01 09:22:47 +1100

    ✨  `this` highlighting for typescript

---
+++ code-examples/.ts
@@ -0,0 +1,26 @@
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+var mySearch: SearchFunc;
+mySearch = function(source: string, subString: string) {
+  var result = source.search(subString);
+  if (result == -1) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+var greeter = new Greeter("world");
