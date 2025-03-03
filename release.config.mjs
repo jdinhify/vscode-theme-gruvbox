a307ba088b4019b130490453c1c4bbcb78a1fb99
jd <14046273+jdinhify@users.noreply.github.com>
2025-03-03 23:16:06 +1100

    use github as the source of release & changelog
    ignore node_modules when publishing
    
    x

--- release.config.mjs
+++ release.config.mjs
@@ -6,21 +6,6 @@ export default {
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
-		[
-			"@semantic-release/changelog",
-			{
-				changelogFile: "CHANGELOG.md",
-			},
-		],
-		"@semantic-release/npm",
-		[
-			"@semantic-release/git",
-			{
-				assets: ["package.json", "CHANGELOG.md"],
-				message:
-					"chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-			},
-		],
 		"@semantic-release/github",
 	],
 };
